172742e75bf94dab7201ec3e5b2d0a767e8ec0bd
just1ngray <just1ngray@users.noreply.github.com>
2020-03-02 14:57:48 -0400

    Navigation handled in less, more versitile functions

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,57 +1,64 @@
 // Description: This file contains functions that link the header fields on compose.html to help pages.
 // Author: Vitor Jeronimo (A00431599)
 
+/*
+* Navigates the user to a specific page.
+* @param href the page to navigate to
+* @returns    NA
+*/
+function goTo(href) {
+  try {
+    localStorage.setItem("lastPage", window.location.href);
+  } catch (error) {
+    console.log("Could not save lastPage");
+  }
 
-// || Navigation
+  window.location.href = href;
+}
 
-$("#btn_sentitems").on("click", function() {
-    window.location.href = "sentitems.html";
-});
-
-$("#btn_compose").on("click", function() {
-    window.location.href = "compose.html";
-});
-
-$("#btn_inbox").on("click", function() {
-    window.location.href = "index.html";
-});
-
-$("#btn_sentitems").on("click", function() {
-    window.location.href = "sentitems.html";
-});
-
-$("#btn_cancel").on("click", function() {
+/*
+* Takes the user back one page. Refreshes the page if possible.
+* @returns  NA
+*/
+function goBack() {
+  if (typeof (window.Storage) === "undefined"){
+		// storage not supported by browser
     window.history.back();
-});
-
-$("#btn_back").on("click", function() {
+  } else if (localStorage.getItem("lastPage") != null){
+    // valid page found
+	  window.location.href = localStorage.getItem("lastPage");
+  } else {
+    // use in-browser history
     window.history.back();
-});
+  }
+}
 
+/*
+* Opens a help window for a label at an id.
+* @param id the element that stores the label for the help window
+* @returns  NA
+*/
+function openHelp(id) {
+  // the text inside the label of the id
+  var label = $("#" + id);
 
-
-// || Help Links
-
-// Opens a help page with info about the header field "To"
-// Returns: N/A
-$("#email_to_label").on("click", function() {
-    window.open("./help/helpto.html", "_blank", "width=500, height=200, left=300, top=250");
-});
-
-// Opens a help page with info about the header field "Cc"
-// Returns: N/A
-$("#email_cc_label").on("click", function() {
-    window.open("./help/helpcc.html", "_blank", "width=500, height=200, left=300, top=250");
-});
-
-// Opens a help page with info about the header field "Subject"
-// Returns: N/A
-$("#email_subject_label").on("click", function() {
-    window.open("./help/helpsubject.html", "_blank", "width=500, height=200, left=300, top=250");
-});
-
-// Opens a help page with info about the header field "Body"
-// Returns: N/A
-$("#email_body_label").on("click", function() {
-    window.open("./help/helpbody.html", "_blank", "width=500, height=200, left=300, top=250");
-});
+  // depending on the valid of label, open a different window
+  switch (label) {
+    case "To":
+      window.open("./help/helpto.html", "_blank", "width=500, height=200, left=300, top=250");
+      break;
+    case "From":
+      break;
+    case "Cc":
+      window.open("./help/helpcc.html", "_blank", "width=500, height=200, left=300, top=250");
+      break;
+    case "Subject":
+      window.open("./help/helpsubject.html", "_blank", "width=500, height=200, left=300, top=250");
+      break;
+    case "Body":
+      window.open("./help/helpbody.html", "_blank", "width=500, height=200, left=300, top=250");
+      break;
+    default:
+      console.error("Help window not found for given label at id:" + id);
+  }
+}
