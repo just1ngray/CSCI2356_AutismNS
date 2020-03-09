f0a8e117043e0457b8ca65b90c7f2b4020a6fda5
just1ngray <just1ngray@users.noreply.github.com>
2020-03-08 23:48:26 -0300

    Documentation

--- scripts/accountManager.js
+++ scripts/accountManager.js
@@ -1,4 +1,15 @@
 /*
+* This file manages accounts in our email system. This account method is better
+* than storing individual hardcoded arrays since this system would be able to
+* handle many accounts without much modification. Room for expansion is
+* important.
+*
+* @author Justin Gray (A00426753)
+* @author Vitor Jeronimo
+* @author Jay Patel
+*/
+
+/*
 * Creates a new account object (for either admin or student or other?)
 *
 * USAGE:
@@ -17,26 +28,38 @@ function Account(name, inboxMail, sentMail) {
   this.sentMail = sentMail;
 }
 
+/*
+* Saves multiple accounts (but only if each CAN be saved). Used when an email
+* is being saved, hense the sender and recipient parameters.
+* @param sender           the sender of the email
+* @param recipient        the recipient of the email
+* @throws MailSaveError   if one or both of the mail boxes is full
+* @returns                NA
+*/
 function saveAccounts(sender, recipient) {
+  // make sure the recipient has room to recieve the email
   if (recipient.inboxMail.length > 10) {
     throw new MailSaveError("Their inbox is full!\n"
       + "They need to delete some emails to make room.");
   }
+  // make sure the sender has room to store the sent mail
   if (sender.sentMail.length > 10) {
     throw new MailSaveError("Your sent mail is full!\n"
       + "You need to delete some emails to make room.");
   }
+  // save both accounts individually (these can throw MailSaveErrors too)
   saveAccount(sender);
   saveAccount(recipient);
 }
 
 /*
-* Saves an account to local storage.
-* @param account  the account to save to local storage
-* @returns        N/A
+* Saves an account.
+* @param account        the account to save
+* @throws MailSaveError if one or both of their boxes is full
+* @returns              NA
 */
 function saveAccount(account) {
-  // caps insensitive: easier sign-in
+  // caps insensitive
   account.name = account.name.toLowerCase();
 
   // sort inbox in descending order by date (newest at top)
@@ -72,7 +95,7 @@ function saveAccount(account) {
 }
 
 /*
-* Gets an account stored in local storage.
+* Gets an account.
 * If no account is found under that name, it returns an empty account of that
 * name.
 * @param name the name of the account (must be unique)
