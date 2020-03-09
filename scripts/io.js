1721a3c2775127f3d24949dc32a614d8864995b1
just1ngray <just1ngray@users.noreply.github.com>
2020-03-09 00:10:50 -0300

    Documentation

--- scripts/io.js
+++ scripts/io.js
@@ -1,3 +1,19 @@
+/*
+* This file handles all the persistent storage for our email system. Right now
+* it uses localStorage only, but the site can be changed over to a server
+* storage solution in this file alone.
+*
+* All localStorage read/writing that does not call io.js is very temporary and
+* will never involve a server.
+*
+* @author Justin Gray (A00426753)
+* @author Vitor Jeronimo
+* @author Jay Patel
+*/
+
+/*
+* Reads data from storage by a key.
+*/
 function read(key) {
   if (typeof (window.Storage) === "undefined"){
 		// storage not supported by browser
@@ -8,6 +24,10 @@ function read(key) {
   }
 }
 
+/*
+* Writes data into storage using a key value pair. Value is written at location
+* key. Just like a map.
+*/
 function write(key, value) {
   try {
     localStorage.setItem(key, value);
