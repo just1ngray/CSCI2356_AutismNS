fdbb97466a85a311c238b1fda610977580f2a540
just1ngray <just1ngray@users.noreply.github.com>
2020-03-09 00:13:42 -0300

    Documentation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,10 @@
-// Description: This file contains functions that link the header fields on compose.html to help pages.
-// Author: Vitor Jeronimo (A00431599)
+/*
+* This file manages navigation in our email system.
+*
+* @author Justin Gray (A00426753)
+* @author Vitor Jeronimo
+* @author Jay Patel
+*/
 
 /*
 * Navigates the user to a specific page.
@@ -7,17 +12,19 @@
 * @returns    NA
 */
 function goTo(href) {
+  // write current page into localStorage for going back functionality
   try {
     localStorage.setItem("lastPage", window.location.href);
   } catch (error) {
     console.log("Could not save lastPage");
   }
 
+  // go to the new href
   window.location.href = href;
 }
 
 /*
-* Takes the user back one page. Refreshes the page if possible.
+* Takes the user back one page. Refreshes the page if needed.
 * @returns  NA
 */
 function goBack() {
@@ -40,25 +47,29 @@ function goBack() {
 */
 function openHelp(id) {
   // the text inside the label of the id
-  //var label = $("#" + id).val();
   var label = document.getElementById(id).innerHTML;
 
   // depending on the valid of label, open a different window
   switch (label) {
     case "To":
-      window.open("../help/helpto.html", "_blank", "width=500, height=200, left=300, top=250");
+      window.open("../help/helpto.html", "_blank",
+        "width=500, height=200, left=300, top=250");
       break;
     case "From":
-      window.open("../help/helpfrom.html", "_blank", "width=500, height=200, left=300, top=250");
+      window.open("../help/helpfrom.html", "_blank",
+        "width=500, height=200, left=300, top=250");
       break;
     case "Cc":
-      window.open("../help/helpcc.html", "_blank", "width=500, height=200, left=300, top=250");
+      window.open("../help/helpcc.html", "_blank",
+        "width=500, height=200, left=300, top=250");
       break;
     case "Subject":
-      window.open("../help/helpsubject.html", "_blank", "width=500, height=200, left=300, top=250");
+      window.open("../help/helpsubject.html", "_blank",
+        "width=500, height=200, left=300, top=250");
       break;
     case "Body":
-      window.open("../help/helpbody.html", "_blank", "width=500, height=200, left=300, top=250");
+      window.open("../help/helpbody.html", "_blank",
+        "width=500, height=200, left=300, top=250");
       break;
     default:
       console.error("Help window not found for " + label + " at id:" + id);
