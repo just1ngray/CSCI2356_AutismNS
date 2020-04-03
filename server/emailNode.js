b164662aa08f17cdf80474f099fb3ebda0d775fc
just1ngray <just1ngray@users.noreply.github.com>
2020-04-03 17:56:37 -0300

    Functional mongo interfacing

--- server/emailNode.js
+++ server/emailNode.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var server = express();
+var MongoClient = require("mongodb").MongoClient;
+
 var PORT = 3355;
+var URL = 'mongodb://jp_gray:A00426753@127.0.0.1:27017/jp_gray';
 
 server.use(express.json());
 server.use(express.urlencoded({
@@ -24,44 +27,106 @@ server.listen(PORT, function() {
   console.log('Now listening for activity on port ' + PORT);
 });
 
-server.post("/account", function(req, res) {
+server.post("/getAccount", function(req, res) {
   var accName = req.body.name;
-  console.log('Request received for account ' + accName);
-  // get all emails (sorted by date) from mongo db collection by accName
+  console.log('Request received to get account: ' + accName);
 
-  var fakeIn = {
-    fakeFrom: "professor@smu.ca",
-    fakeTo: "student@smu.ca",
-    cc: "The rest of the class!",
-    subject: "COVID19 Response",
-    body: "You all fail. Muhahah!",
-    isRead: false,
-    realFrom: "admin",
-    realTo: "student",
-    date: (new Date()).getTime(),
-    owner: "student",
-    isInbox: true
-  }
+  // get the account object from the mongodb
+  MongoClient.connect(URL, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db('jp_gray');
+    dbo.collection('accounts').findOne({name: accName}, function(err, result) {
+      if (err) throw err;
 
-  var fakeOut = {
-    fakeFrom: "student",
-    fakeTo: "McDonalds@ImHungry.com",
-    cc: "",
-    subject: "Can I has Cheeseburger?",
-    body: "Please, could I please pls has Cheeseburger?!",
-    isRead: false,
-    realFrom: "student",
-    realTo: "admin",
-    date: (new Date()).getTime(),
-    owner: "student",
-    isInbox: false
-  }
-
-  var account = {
-    name: accName,
-    inboxMail: [fakeIn],
-    sentMail: [fakeOut]
-  };
-
-  return res.status(200).send(account);
+      // no document found
+      if (result == null) {
+        result = {
+          name: accName,
+          inboxMail: [],
+          sentMail: []
+        };
+        console.log('No document in accounts by the name ' + accName
+          + ' creating an empty account');
+      } else {
+        console.log('Found ' + result.name + ' in accounts');
+      }
+      db.close();
+      return res.status(200).send(result);
+    });
+  });
 });
+
+server.post("/writeAccount", function(req, res) {
+  var accName = req.body.name;
+  console.log('Request received to write account: ' + accName);
+
+  // get the account object from the mongodb
+  MongoClient.connect(URL, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db('jp_gray');
+    dbo.collection('accounts').replaceOne({name: accName}, req.body,
+      {upsert: true}, function(err, result) {
+
+      if (err) throw err;
+      console.log('Updated account ' + accName + ' in accounts');
+      db.close();
+      return res.status(200);
+    });
+  });
+});
+
+var fakeIn = {
+  fakeFrom: "professor@smu.ca",
+  fakeTo: "student@smu.ca",
+  cc: "The rest of the class!",
+  subject: "COVID19 Response",
+  body: "You all fail. Muhahah!",
+  isRead: false,
+  realFrom: "admin",
+  realTo: "student",
+  date: (new Date()).getTime(),
+  owner: "student",
+  isInbox: true
+}
+
+var fakeOut = {
+  fakeFrom: "student",
+  fakeTo: "McDonalds@ImHungry.com",
+  cc: "",
+  subject: "Can I has Cheeseburger?",
+  body: "Please, could I please pls has Cheeseburger?!",
+  isRead: false,
+  realFrom: "student",
+  realTo: "admin",
+  date: (new Date()).getTime(),
+  owner: "student",
+  isInbox: false
+}
+
+var testAccount = {
+  name: 'testAccount',
+  inboxMail: [fakeIn],
+  sentMail: [fakeOut]
+};
+
+// inserts into a collection
+// MongoClient.connect(URL, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db('jp_gray');
+//   dbo.collection('accounts').insertOne(testAccount, function(err, res) {
+//     if (err) throw err;
+//     console.log('Account (' + testAccount.name + ') inserted!');
+//     db.close();
+//   });
+// });
+
+// creates a collection
+// MongoClient.connect(URL, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db('jp_gray');
+//   dbo.createCollection('accounts', function(err, res) {
+//     if (err) throw err;
+//     console.log('Collection created!');
+//     db.close();
+//   });
+// });
