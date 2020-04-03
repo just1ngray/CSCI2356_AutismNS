a954a61372236c77b22a52d5cc647289e42bd417
just1ngray <just1ngray@users.noreply.github.com>
2020-04-03 20:55:55 -0300

    Made it function from the back end

--- server/emailNode.js
+++ server/emailNode.js
@@ -23,17 +23,20 @@ var allowCrossDomain = function (req, res, next) {
 }
 server.use(allowCrossDomain);
 
+////////////////////////////////////////////////////////////////////////////////
+
 server.listen(PORT, function() {
   console.log('Now listening for activity on port ' + PORT);
 });
 
 server.post("/getAccount", function(req, res) {
-  var accName = req.body.name;
-  console.log('Request received to get account: ' + accName);
-
   // get the account object from the mongodb
   MongoClient.connect(URL, function(err, db) {
     if (err) throw err;
+
+    var accName = req.body.name;
+    console.log('Request received to get account: ' + accName);
+
     var dbo = db.db('jp_gray');
     dbo.collection('accounts').findOne({name: accName}, function(err, result) {
       if (err) throw err;
@@ -41,13 +44,26 @@ server.post("/getAccount", function(req, res) {
       // no document found
       if (result == null) {
         result = {
+          _id: accName,
           name: accName,
           inboxMail: [],
           sentMail: []
         };
         console.log('No document in accounts by the name ' + accName
           + ' creating an empty account');
+
+        dbo.collection('accounts').insertOne(result, function(err, res) {
+          if (err) throw err;
+          console.log('Inserted account ' + accName + ' into accounts')
+        });
       } else {
+        // empty arrays are not stored in mongo so they have to be added later
+        if (result.inboxMail == null) {
+          result.inboxMail = [];
+        }
+        if (result.sentMail == null) {
+          result.sentMail = [];
+        }
         console.log('Found ' + result.name + ' in accounts');
       }
       db.close();
@@ -57,15 +73,17 @@ server.post("/getAccount", function(req, res) {
 });
 
 server.post("/writeAccount", function(req, res) {
-  var accName = req.body.name;
-  console.log('Request received to write account: ' + accName);
-
   // get the account object from the mongodb
   MongoClient.connect(URL, function(err, db) {
     if (err) throw err;
+
+    var accName = req.body.name;
+    console.log('WRINTING: ' + accName);
+
     var dbo = db.db('jp_gray');
-    dbo.collection('accounts').replaceOne({name: accName}, req.body,
-      {upsert: true}, function(err, result) {
+    dbo.collection('accounts').replaceOne({
+        name: accName
+      }, req.body, function(err, result) {
 
       if (err) throw err;
       console.log('Updated account ' + accName + ' in accounts');
@@ -75,39 +93,39 @@ server.post("/writeAccount", function(req, res) {
   });
 });
 
-var fakeIn = {
-  fakeFrom: "professor@smu.ca",
-  fakeTo: "student@smu.ca",
-  cc: "The rest of the class!",
-  subject: "COVID19 Response",
-  body: "You all fail. Muhahah!",
-  isRead: false,
-  realFrom: "admin",
-  realTo: "student",
-  date: (new Date()).getTime(),
-  owner: "student",
-  isInbox: true
-}
-
-var fakeOut = {
-  fakeFrom: "student",
-  fakeTo: "McDonalds@ImHungry.com",
-  cc: "",
-  subject: "Can I has Cheeseburger?",
-  body: "Please, could I please pls has Cheeseburger?!",
-  isRead: false,
-  realFrom: "student",
-  realTo: "admin",
-  date: (new Date()).getTime(),
-  owner: "student",
-  isInbox: false
-}
-
-var testAccount = {
-  name: 'testAccount',
-  inboxMail: [fakeIn],
-  sentMail: [fakeOut]
-};
+// var fakeIn = {
+//   fakeFrom: "professor@smu.ca",
+//   fakeTo: "student@smu.ca",
+//   cc: "The rest of the class!",
+//   subject: "COVID19 Response",
+//   body: "You all fail. Muhahah!",
+//   isRead: false,
+//   realFrom: "admin",
+//   realTo: "student",
+//   date: (new Date()).getTime(),
+//   owner: "student",
+//   isInbox: true
+// }
+//
+// var fakeOut = {
+//   fakeFrom: "student",
+//   fakeTo: "McDonalds@ImHungry.com",
+//   cc: "",
+//   subject: "Can I has Cheeseburger?",
+//   body: "Please, could I please pls has Cheeseburger?!",
+//   isRead: false,
+//   realFrom: "student",
+//   realTo: "admin",
+//   date: (new Date()).getTime(),
+//   owner: "student",
+//   isInbox: false
+// }
+//
+// var testAccount = {
+//   name: 'testAccount',
+//   inboxMail: [fakeIn],
+//   sentMail: [fakeOut]
+// };
 
 // inserts into a collection
 // MongoClient.connect(URL, function(err, db) {
